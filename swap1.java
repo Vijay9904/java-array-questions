6e89028362b3e3fdc53379374987355a8bb6db4b
khetre vijay <151039014+Vijay9904@users.noreply.github.com>
2024-07-31 10:23:16 -0700

    swap array values and reverse array
    swap array values and reverse array

---
+++ swap1.java
@@ -0,0 +1,30 @@
+import  java.util.Arrays;
+
+class swap1{
+    public static void main(String[] args) {
+        int[] v={1,3,23,9,10};
+        //swap(v,1,3);
+        reverse(v);
+        System.out.println(Arrays.toString(v));
+    }
+    // swaping values in an array
+     static void swap1(int[] v, int index1, int index2)
+     {
+         int temp=v[index1];
+         v[index1]=v[index2];
+         v[index2]=temp;
+     }
+     //reverse the array
+     static void reverse(int[]v){
+        int start=0;
+        int end=v.length-1;
+        while (start<=end){
+        swap1(v,start,end);
+                start++;
+                end--;
+
+         }
+
+     }
+
+}
